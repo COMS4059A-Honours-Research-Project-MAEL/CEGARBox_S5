40fd49b2208133dae7aed4b084619e012c858bf6
NtandoRaji <2584925@students.wits.ac.za>
2025-08-07 22:14:04 +0200

    Removed ParseFormulaNew

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@
 #include "Formula/Or/Or.h"
 #include "Formula/True/True.h"
 #include "ParseFormula/ParseFormula.h"
-#include "ParseFormulaNew/ParseFormulaNew.h"
+// #include "ParseFormulaNew/ParseFormulaNew.h"
 #include "Prover/TrieformProver/TrieformProverK/TrieformProverK.h"
 #include "Prover/TrieformProver/TrieformProverKDag/TrieformProverKDag.h"
 #include "Prover/TrieformProver/TrieformProverKGlobal/TrieformProverKGlobal.h"
