8cc162378c073982d25a7d7c5e824deba9949c06
NtandoRaji <2584925@students.wits.ac.za>
2025-11-22 00:35:56 +0200

    Updated README.md and added examples

--- Examples/tests.sh
+++ Examples/tests.sh
@@ -1,4 +1,6 @@
 ./../CEGARBox_S5 -f Kaxiom.txt
-./../CEGARBox_S5 -f Kaxiom.txt -n
-./../CEGARBox_S5 -f 4axiom.txt
-./../CEGARBox_S5 -f 4axiom.txt -4
+./../CEGARBox_S5 -f Taxiom.txt 
+./../CEGARBox_S5 -f 4axiom.txt 
+./../CEGARBox_S5 -f S5axiom.txt
+./../CEGARBox_S5 -f S5axiom.txt -a  
+./../CEGARBox_S5 -f sample.txt -m
